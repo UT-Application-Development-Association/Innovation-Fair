e276e8e25eb1e9a108295690e5a45644c3b64298
rainyuxuan <yu2001xuan@outlook.com>
2020-06-23 14:32:14 -0400

    changed header style in js

--- js/index.js
+++ js/index.js
@@ -3,35 +3,43 @@ if (window.innerWidth <= 768){
     document.querySelector('#navToggleBtn').hidden = false;
     document.querySelector('#navSignupMobile').hidden = false;
     document.querySelector('#navSignup').hidden = true;
-    document.querySelector('#navLinks').style.flexDirection = 'column';
-    document.querySelector('#navLinks').style.justifyContent = 'center';
-    document.querySelector('#navLinks').style.marginLeft = 'auto';
+    document.querySelector('#navList').style.flexDirection = 'column';
+    document.querySelector('#navList').style.justifyContent = 'center';
+    document.querySelector('#navList').style.marginLeft = 'auto';
     
 }else{
     document.querySelector('#navSignupMobile').hidden = true;
-    document.querySelector('#navLinks').style.flexDirection='row';
+    document.querySelector('#navList').style.flexDirection='row';
 }
 // adjust navbar according to scroll
 window.onscroll = () => {
-    if (window.scrollY >= (document.querySelector('#headImageCarousel').offsetHeight) * 0.8){
+    if (window.scrollY >= (document.querySelector('#headImageCarousel').offsetHeight) * 0.9){
         // When scrolled down
         document.querySelector('#navBrand').hidden = false;
-        // document.querySelector('#navSignup').hidden = false;
         document.querySelector('#navToggleBtn').hidden = false;
+        // if not mobile device
         if(window.innerWidth > 768){
             document.querySelector('#navToggleBtn').hidden = true;
-            document.querySelector('#navLinks').style.justifyContent='start';
+            document.querySelector('#navList').style.justifyContent='start';
         }else{
-            document.querySelector('#navLinks').style.justifyContent='center';
+            document.querySelector('#navList').style.justifyContent='center';
         }
+        
     } else {
         // When focusing on headImage
         document.querySelector('#navBrand').hidden = true;
         // if not mobile device
         if(window.innerWidth > 768){
             document.querySelector('#navToggleBtn').hidden = true;
-            document.querySelector('#navLinks').style.justifyContent='space-evenly';
+            document.querySelector('#navList').style.justifyContent='space-evenly';
         }
+    }
+
+    // header box shadow
+    if (window.scrollY >= (document.querySelector('#headImageCarousel').offsetHeight) * 1.1){
+        document.querySelector('header').style.boxShadow = "0 5px 2px -2px rgba(216,220,219,0.4)";
+    } else {
+        document.querySelector('header').style.boxShadow = "none";
     }
 }
 
